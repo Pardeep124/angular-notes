a6e60332c4b47178fea1c545235bd9cd2df6af5e
Pardeep <pardeep942001@gmail.com>
2022-01-14 14:18:29 +0530

    netlify routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,23 +4,28 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [{
-  path: 'home',
-  component: HomeComponent
-}, {
-  path: 'about',
-  component: AboutComponent
-},{
-  path: 'contact',
-  component: ContactComponent
-},{
-  path: '',
-  redirectTo: '/home',
-  pathMatch: 'full'
-}];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
